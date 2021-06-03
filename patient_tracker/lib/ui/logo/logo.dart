646c9d4561a23a28c72103143a7661d8399037aa
MARWA HUSSEIN <miroohussein98@gmail.com>
2021-06-03 22:00:19 +0200

    logo1

--- patient_tracker/lib/ui/logo/logo.dart
+++ patient_tracker/lib/ui/logo/logo.dart
@@ -8,38 +8,66 @@ class Logo extends StatefulWidget{
 
 }
 class _Logo extends State<Logo> with SingleTickerProviderStateMixin{
- late AnimationController _animationController;
+ //late AnimationController _animationController;
+ double _height =190;
+ double _width = 190;
+ bool _visible= true;
   @override
-  void initState() {
-    super.initState();
-    _animationController =AnimationController(
-      duration: Duration(microseconds: 200), vsync: this
-    );
-
-    _animationController.forward();
-    _animationController.addListener(() {
-      print(_animationController.value);
-    });
-  }
+  // void initState() {
+  //   super.initState();
+  //   _animationController =AnimationController(
+  //     duration: Duration(microseconds: 200), vsync: this
+  //   );
+  //
+  //   _animationController.forward();
+  //   _animationController.addListener(() {
+  //     print(_animationController.value);
+  //   });
+  // }
   @override
-  void dispose() {
-    super.dispose();
-    _animationController.dispose();
-  }
+  // void dispose() {
+  //   super.dispose();
+  //   _animationController.dispose();
+  // }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
-        alignment: AlignmentDirectional.center,
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
-
           children: [
-            Container(
-              height: 190,
-              width: 190,
-              child: Image.asset("assets/images/logo.png",fit: BoxFit.cover,),
+            Center(
+              child: AnimatedOpacity(
+                opacity:_visible==true? 1:0 ,
+                duration: Duration(seconds: 3),
+                child: AnimatedContainer(
+                  curve: Curves.linear,
+                  duration: Duration(seconds: 3),
+                  child: Container(
+                    alignment: AlignmentDirectional.center,
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        Container(
+                          height: _height,
+                          width: _width,
+                          child: Image.asset("assets/images/logo.png",fit: BoxFit.cover,),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+            ),
+            FloatingActionButton(
+                child: Icon(Icons.refresh_sharp,),
+                onPressed:(){
+                  setState((){
+                    _height =_height == 190? 700:190;
+                    _width = _width == 190? 700:190;
+                    _visible =!_visible;
+                  });
+                }
             ),
           ],
         ),
